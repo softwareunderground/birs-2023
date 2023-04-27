3a12eabf26c04a507271ba3d6a9124a087e0de30
Santiago Soler <santisoler@fastmail.com>
2023-04-27 16:29:07 -0700

    Make theme change persistent through the website (#5)
    Store the theme configuration in `localStorage` so it's persistent
    through the website. Add a `title` to the theme switcher button that
    gets updated when the theme changes.

--- static/js/theme-switch.js
+++ static/js/theme-switch.js
@@ -1,34 +1,54 @@
+// Get theme button by id
 const themeButton = document.getElementById("themeSwitcher");
 
-function toggleTheme(theme) {
-    if (theme == "auto") {
-        document.body.removeAttribute("data-theme");
-    } else {
-        document.body.setAttribute("data-theme", theme);
-    }
+// Check if there's a persistent configuration of the website's theme
+let theme = localStorage.getItem("theme");
+if (theme) {
+  toggleTheme(theme);
+}
+
+function setTheme(theme) {
+  // Change the data-theme attribute of the page
+  if (theme == "auto") {
+    document.body.removeAttribute("data-theme");
+  } else {
+    document.body.setAttribute("data-theme", theme);
+  }
 }
 
 function updateIcon(theme) {
-    let icon = themeButton.querySelector("i");
-    icon.removeAttribute("class");
-    if (theme == "dark") {
-        icon.setAttribute("class", "fa-solid fa-moon");
-    } else if (theme == "light") {
-        icon.setAttribute("class", "fa-solid fa-sun");
-    } else if (theme == "auto") {
-        icon.setAttribute("class", "fa-solid fa-circle-half-stroke");
-    }
+  // Update icon of the theme switcher button
+  if (theme == "dark") {
+    fa_icon_class = "fa-solid fa-moon";
+  } else if (theme == "light") {
+    fa_icon_class = "fa-solid fa-sun";
+  } else {
+    fa_icon_class = "fa-solid fa-circle-half-stroke";
+  }
+  icon = themeButton.querySelector("i");
+  icon.removeAttribute("class");
+  icon.setAttribute("class", fa_icon_class);
+  icon.setAttribute("title", "Toggle theme" + " (" + theme + ")");
+}
+
+function toggleTheme(theme) {
+  // Toggle website's theme
+  //
+  // Changes data-theme attribute of the page, updates button icon and saves
+  // the current configuration to localStorage so the change is persistent
+  setTheme(theme);
+  updateIcon(theme);
+  localStorage.setItem("theme", theme);
 }
 
 themeButton.addEventListener("click", () => {
   theme = "auto";
   if (!document.body.hasAttribute("data-theme")) {
-      theme = "dark";
+    theme = "dark";
   } else if (document.body.getAttribute("data-theme") == "dark") {
-      theme = "light";
+    theme = "light";
   } else if (document.body.getAttribute("data-theme") == "light") {
-      theme = "auto";
+    theme = "auto";
   }
   toggleTheme(theme);
-  updateIcon(theme);
 });
